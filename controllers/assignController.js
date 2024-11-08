c92dc03cc645e4a0faba0a4e91924c39f15e19bb
Palvin Muthesh <spalvinmuthesh@gmail.com>
2024-11-08 15:39:45 +0530

    today

--- controllers/assignController.js
+++ controllers/assignController.js
@@ -1,6 +1,6 @@
 const ContainerAssign = require('../models/containerAssignModel');
 const CookerAssign = require('../models/cookerAssignModel');
-const TruckAssign = require('../models/containerAssignModel');
+const TruckAssign = require('../models/truckAssignModel');
 const ErrorHandler = require('../utils/ErrorHandler');
 const catchAsyncError = require('../middleware/CatchAsyncErrors');
 const cloudinary = require('../config/cloudinary');
@@ -27,6 +27,33 @@ exports.createContainerAssign = catchAsyncError(async (req, res, next) => {
 exports.createTruckAssign = catchAsyncError(async (req, res, next) => {
   // req.body.admin = req.user.id;
   const truck = await TruckAssign.create(req.body);
+  res.status(200).json({
+    success: true,
+    data: truck,
+  });
+});
+
+exports.getCookerAssign = catchAsyncError(async (req, res, next) => {
+  console.log(req, "RRRRRRRRRRRR")
+  const truck = await CookerAssign.find({});
+  res.status(200).json({
+    success: true,
+    data: truck,
+  });
+});
+
+exports.getContainerAssign = catchAsyncError(async (req, res, next) => {
+  console.log(req, "RRRRRRRRRRRR")
+  const truck = await ContainerAssign.find({});
+  res.status(200).json({
+    success: true,
+    data: truck,
+  });
+});
+
+exports.getTruckAssign = catchAsyncError(async (req, res, next) => {
+  console.log(req, "RRRRRRRRRRRR")
+  const truck = await TruckAssign.find({});
   res.status(200).json({
     success: true,
     data: truck,
